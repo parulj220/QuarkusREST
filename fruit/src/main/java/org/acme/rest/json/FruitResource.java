7e9a2757cb18b0837f2a81f3bff44358d60ba643
parulj220 <105209961+parulj220@users.noreply.github.com>
2022-05-11 18:09:27 +0530

    Add files via upload

---
+++ fruit/src/main/java/org/acme/rest/json/FruitResource.java
@@ -0,0 +1,43 @@
+package org.acme.rest.json;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/fruits")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class FruitResource {
+
+        private Set<Fruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+
+        public FruitResource() {
+            fruits.add(new Fruit("Apple", "Winter fruit"));
+            fruits.add(new Fruit("Pineapple", "Tropical fruit"));
+        }
+
+    @GET
+        public Response list() {
+        return Response.ok(fruits).build();
+    }
+       /* @GET
+        public Set<Fruit> list() {
+            return fruits;
+        }*/
+
+        @POST
+        public Set<Fruit> add(Fruit fruit) {
+            fruits.add(fruit);
+            return fruits;
+        }
+
+        @DELETE
+        public Set<Fruit> delete(Fruit fruit) {
+            fruits.removeIf(existingFruit -> existingFruit.name.contentEquals(fruit.name));
+            return fruits;
+        }
+
+}
